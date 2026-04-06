df6468d2a498bc81fc6405646dc39dc0eab2cb09
Науменко Дмитрий <AridanWarlock@gmail.com>
2026-04-07 01:30:09 +0300

    feat: добавлен тип Nullable[T] для опциональных json полей

---
+++ internal/transport/http/request/nullable.go
@@ -0,0 +1,26 @@
+package http_request
+
+import (
+	"encoding/json"
+
+	"github.com/AridanWarlock/pinnAutomizer/internal/domain"
+)
+
+type Nullable[T any] struct {
+	domain.Nullable[T]
+}
+
+func (n *Nullable[T]) UnmarshalJSON(b []byte) error {
+	if string(b) == "null" {
+		return nil
+	}
+
+	n.Set = true
+	var val T
+	if err := json.Unmarshal(b, &val); err != nil {
+		return err
+	}
+
+	n.Value = &val
+	return nil
+}
